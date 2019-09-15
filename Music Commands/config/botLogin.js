c56dffc4b599e1a0105cf97ddb23f11d088b7940
yhysaaaaaaaaaaaaaaa <55056419+yhysaaaaaaaaaaaaaaa@users.noreply.github.com>
2019-09-15 20:18:46 +0300

    Add files via upload

---
+++ Music Commands/config/botLogin.js
@@ -0,0 +1,18 @@
+// The bot only requires a token if you have a bot account
+// You can get a bot token at https://discordapp.com/developers/applications/me
+
+// Youtube API is needed to use the search features
+// otherwise you will keep getting bad request
+
+// Owner id is required to use the admin commands
+// you can get this id if you have developer options enabled
+// in discord. Then just right click your username and copy id
+
+// **WARNING** If you do not add a Owner ID or have a group that is in the "preference.json" file
+// Then no one will be able to use the admin commands
+
+module.exports = {
+	token: "NjIwNjMxNDk2NDgyNjE5NDAz.XX5u_Q.ptkfJC5tihjjBse8bLVNYKOQPuU",
+	youtubeAPI: "AIzaSyAdORXg7UZUo7sePv97JyoDqtQVi3Ll0b8",
+	owner_id: "523954150166495255"
+}
